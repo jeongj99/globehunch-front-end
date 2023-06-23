062c29d0d3a8729d2dbd2e1c92023153f03f1cb5
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-06-23 14:59:13 -0400

    Create new repository to separate front and back end

---
+++ src/components/Tutorial/TutorialVideo.js
@@ -0,0 +1,9 @@
+
+export default function Video () {
+
+  return (
+    <>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/XQQf1xKwkxY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
+    </>
+  )
+}
