0d618265728b8674875891185058fd87eada0c4f
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-06-28 23:01:04 -0400

    Create an AuthProvider context

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect } from "react";
+import axios from '../api/axios';
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [isReady, setIsReady] = useState(false);
+
+  const authenticate = async () => {
+    try {
+      const response = await axios.post('api/authenticate', {});
+      setAuth(response.data.authenticated);
+      setLoggedInUser(response.data.user);
+    } catch (error) {
+      console.log(error);
+      setAuth(false);
+      setLoggedInUser(null);
+    }
+  };
+  setIsReady(true);
+
+  useEffect(() => {
+    authenticate();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth, loggedInUser, setLoggedInUser }}>
+      {isReady && children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
