ca642809cf6730c6c0b84ba7562c653186479d99
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-06-29 22:26:22 -0400

    Remove props from Login and Register components

--- src/App.js
+++ src/App.js
@@ -15,34 +15,16 @@ import Tutorial from "./pages/Tutorial";
 import './App.css';
 
 function App() {
-  const { auth, loggedInUser } = useContext(AuthContext);
-  // const [user, setUser] = useState(localStorage.getItem('user'));
-  // const [userID, setUserID] = useState(localStorage.getItem('userID'));
-
-  // axios.defaults.withCredentials = true;
-
-  // useEffect(() => {
-  //   axios.post("/api/authenticate", {}).then(response => {
-  //     if (!response.data.error) {
-  //       localStorage.setItem('user', response.data.user.user_name);
-  //       localStorage.setItem('userID', response.data.user.id);
-  //       setUser(response.data.user.user_name);
-  //       setUserID(response.data.user.id);
-  //     } else {
-  //       localStorage.removeItem('user');
-  //       localStorage.removeItem('userID');
-  //     }
-  //   });
-  // });
+  const { auth } = useContext(AuthContext);
 
   return (
     <>
-      <Navbar loggedInUser={user} setUser={setUser} setUserID={setUserID} />
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={auth ? <Navigate to="/" /> : <Login setUser={setUser} setUserID={setUserID} />} />
-          <Route path="/register" element={auth ? <Navigate to="/" /> : <Register setUser={setUser} setUserID={setUserID} />} />
+          <Route path="/login" element={auth ? <Navigate to="/" /> : <Login />} />
+          <Route path="/register" element={auth ? <Navigate to="/" /> : <Register />} />
           <Route path="/leaderboard" element={auth ? <Leaderboard userID={userID} /> : <Navigate to="/login" />} />
           <Route path="/game" element={auth ? <Game userID={userID} /> : <Navigate to="/login" />} />
           <Route path="/tutorial" element={<Tutorial />} />
