84f2497483f83134c452ed60e91b2200d05eb0fd
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-07-04 15:49:16 -0400

    Remove Navbar and moved to individual pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import AuthContext from "./context/AuthProvider";
 
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -18,19 +17,16 @@ function App() {
   const { auth } = useContext(AuthContext);
 
   return (
-    <>
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={auth ? <Navigate to="/" /> : <Login />} />
-          <Route path="/register" element={auth ? <Navigate to="/" /> : <Register />} />
-          <Route path="/leaderboard" element={auth ? <Leaderboard /> : <Navigate to="/login" />} />
-          <Route path="/game" element={auth ? <Game /> : <Navigate to="/login" />} />
-          <Route path="/tutorial" element={<Tutorial />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={auth ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={auth ? <Navigate to="/" /> : <Register />} />
+        <Route path="/leaderboard" element={auth ? <Leaderboard /> : <Navigate to="/login" />} />
+        <Route path="/game" element={auth ? <Game /> : <Navigate to="/login" />} />
+        <Route path="/tutorial" element={<Tutorial />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
