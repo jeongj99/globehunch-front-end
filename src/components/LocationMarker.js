f40b6417fa18519869f99ee9ad4fb453c9342b1e
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-08-17 15:48:14 -0400

    Update using new props

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -1,18 +1,20 @@
 import { Marker, useMapEvents } from 'react-leaflet';
 
 //This component will set a marker on the map wherever the user clicks
-export default function LocationMarker(props) {
+export default function LocationMarker({ gameState, setGameState }) {
 
   const map = useMapEvents({
     //Once user click event happens, set position to the lat and long of where they clicked on map
     click(event) {
-      props.setPosition(event.latlng);
+      const updatedTurns = [...gameState.turns];
+      updatedTurns[gameState.currentTurn - 1].answerPosition = event.latlng;
+      setGameState(prev => ({ ...prev, turns: updatedTurns }));
       map.flyTo(event.latlng);
     }
   });
   //If position has been set by user, return marker component which will mark the map
-  return props.position === null ? null : (
-    <Marker position={props.position}>
+  return gameState.turns[gameState.currentTurn - 1].answerPosition === null ? null : (
+    <Marker position={gameState.turns[gameState.currentTurn - 1].answerPosition}>
     </Marker>
   );
 }
