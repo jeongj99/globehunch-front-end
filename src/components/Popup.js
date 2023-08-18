c0a3980d116d44a2fcb78187fc9e3ca376b2b3ff
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-08-18 15:25:05 -0400

    Change format :

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,10 +4,12 @@ import Background_Video_Pexels from './fireworks.mp4';
 
 export default function Popup(props) {
 
-  return (<div className="popupContainer">
-    <span className={props.messageClass}>{props.message}</span>
-    <video className="popupBackground" src={Background_Video_Pexels} autoPlay loop muted />
-  </div>);
+  return (
+    <div className="popupContainer">
+      <span className={props.messageClass}>{props.message}</span>
+      <video className="popupBackground" src={Background_Video_Pexels} autoPlay loop muted />
+    </div>
+  );
 }
 
 
