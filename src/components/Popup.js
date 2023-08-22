1c62f887e22f9156be27236c464c7ceeab88adc8
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-08-22 11:37:48 -0400

    Update popup message component with new prop

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,13 +2,15 @@ import "./Popup.css";
 
 import Background_Video_Pexels from './fireworks.mp4';
 
-export default function Popup(props) {
+export default function Popup({ gameState }) {
 
   return (
-    <div className="popupContainer">
-      <span className={props.messageClass}>{props.message}</span>
-      <video className="popupBackground" src={Background_Video_Pexels} autoPlay loop muted />
-    </div>
+    gameState.popupMessageStatus && (
+      <div className={`popupContainer ${gameState.popupMessageStatus ? "visible" : ""}`}>
+        <span className="visibleMessage">{`You are ${gameState.turns[gameState.currentTurn - 1].distance}km away.\n Your score is ${gameState.turns[gameState.currentTurn - 1].score}.`}</span>
+        <video className="popupBackground" src={Background_Video_Pexels} autoPlay loop muted />
+      </div>
+    )
   );
 }
 
