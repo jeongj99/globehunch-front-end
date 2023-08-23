4c821e1db4158f8568b2f8593d09447f4bf2120f
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-08-23 16:09:32 -0400

    Add condition and className condtions

--- src/components/NullPositionError.js
+++ src/components/NullPositionError.js
@@ -1,10 +1,13 @@
 import "./NullPositionError.css";
 
-export default function NullPositionError() {
+export default function NullPositionError({ gameState }) {
 
   return (
-    <div className="null-position-popup-container">
-      <h2 className="null-position-popup-title">Warning ⚠️</h2>
-      <span className="null-position-popup-description">Please select a location on the right side map, then click answer.</span>
-    </div>);
+    gameState.errorMessageStatus && (
+      <div className={`null-position-popup-container ${gameState.errorMessageStatus ? "visible" : ""}`}>
+        <h2 className="null-position-popup-title">Warning ⚠️</h2>
+        <span className="null-position-popup-description">Please select a location on the right side map, then click answer.</span>
+      </div>
+    )
+  );
 }
