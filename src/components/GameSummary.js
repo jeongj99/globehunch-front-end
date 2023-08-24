b73853b37de000d2cc08eadfb0a2dbb673acb343
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-08-24 15:52:30 -0400

    Update component by using new prop gameState

--- src/components/GameSummary.js
+++ src/components/GameSummary.js
@@ -3,42 +3,34 @@ import { useNavigate } from "react-router-dom";
 
 import "./GameSummary.css";
 
-export default function GameSummary({ game, playAgain }) {
+export default function GameSummary({ gameState, playAgain }) {
 
   const navigate = useNavigate();
 
-  const navigateToHome = function() {
+  const navigateToHome = () => {
     navigate('/');
   };
 
-  let totalScore = 0;
-  for (let i = 0; i < game.turns.length; i++) {
-    totalScore += game.turns[i].score;
-  }
-
-  let turnElements = game.turns.map((turn, index) => {
+  const turnElements = gameState.turns.map((turn) => {
     return (
       <li>
-        <div>Round {turn.turn_number}</div>
+        <div>Round {turn.turnNumber}</div>
         <div><span className="shine">{turn.score} </span>&nbsp; points</div>
-        <div><span className="shine">{turn.distanceKm}</span>&nbsp; km</div>
+        <div><span className="shine">{turn.distance}</span>&nbsp; km</div>
       </li>
     );
-
   });
 
   return (
     <>
       <div className="summary-container">
         <div className="title"> Game Summary</div>
-        {/* <AnswerMap className="summary-map" /> */}
         <div className="summary-results">
-          <span className="subtitle">Your score is:&nbsp;<span className="shine">{totalScore}</span></span>
+          <span className="subtitle">Your score is:&nbsp;<span className="shine">{gameState.totalScore}</span></span>
           <ul>
             {turnElements}
           </ul>
         </div>
-
         <div className="summary-button-container">
           <Button className="button-summary" title={"Play Again"} onClick={playAgain} />
           <Button className="button-summary" title={"Main Menu"} onClick={navigateToHome} />
